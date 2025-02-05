6c33b0a3c322e1deb40d7b15ba4568998a1dd35d
Jeremy Clark <jeremy@jeremybytes.com>
2025-02-05 14:22:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Visual Studio Live! Las Vegas - March 2025  
+
+## Description  
+This repository contains slides and code samples for sessions presented at Visual Studio Live! Las Vegas - March 10-14, 2025.  
+
+## Sessions  
+
+**W02: DI Why? Getting a Grip on Dependency Injection**  
+*Level: Introductory / Intermediate*  
+
+Many of our modern frameworks have Dependency Injection (DI) built in. But how do you use that effectively? We need to look at what DI is and why we want to use it. We'll look at the problems caused by tight coupling. Then we'll use some DI patterns such as constructor injection to break that tight coupling. We'll see how loosely-coupled applications are easier to extend and test. With a better understanding of the basic patterns, we'll remove the magic behind DI containers so that we can use the tools appropriately in our code.  
+
+You will learn:
+
+* See the problems that DI can solve  
+* Understand DI by using it without a container  
+* See how a DI container can add some magic and reduce some code  
+
+Resources:  
+* Slides: [/W02-dependency-injection/SLIDES-DependencyInjection.pdf](./W02-dependency-injection/SLIDES-DependencyInjection.pdf)
+* Sample Code: [/W02-dependency-injection/](./W02-dependency-injection/)
+
+---
+
+**W17: I'll Get Back to You: Task, Await, and Asynchronous Methods in C#**  
+*Level: Introductory / Intermediate*  
+
+There's a lot of confusion about async/await, Task/TPL, and asynchronous and parallel programming in general. So let's start with the basics and look at how we can consume asynchronous methods using Task and then see how the "await" operator can makes things easier for us. Along the way, we’ll look at continuations, cancellation, and exception handling.
+
+You will learn:
+
+* Get data back from an asynchronous method  
+* Handle exceptions that occur in an asynchronous method  
+* When to use "ConfigureAwait"  
+
+Resources:  
+* Slides: [/W17-task-and-await/SLIDES-TaskAndAwait.pdf](./W17-task-and-await/SLIDES-TaskAndAwait.pdf)
+* Sample Code: [/W17-task-and-await/](./W17-task-and-await/)
+
+---
---
+++ W17-task-and-await/Completed/UsingTask.UI/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using UsingTask.Library;
+using UsingTask.Shared;
+
+namespace UsingTask.UI;
+
+public partial class MainWindow : Window
+{
+    PersonReader reader = new();
+    CancellationTokenSource? tokenSource;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void FetchWithTaskButton_Click(object sender, RoutedEventArgs e)
+    {
+        ClearListBox();
+        tokenSource = new();
+
+        FetchWithTaskButton.IsEnabled = false;
+
+        Task<List<Person>> peopleTask = reader.GetPeopleAsync(tokenSource.Token);
+        peopleTask.ContinueWith(
+            task =>
+            {
+                if (task.IsFaulted)
+                {
+                    foreach(var ex in task.Exception.Flatten().InnerExceptions)
+                    MessageBox.Show($"ERROR\n{ex.GetType()}\n{ex.Message}");
+                }
+                if (task.IsCanceled)
+                {
+                    MessageBox.Show("CANCELED CANCELED CANCELED");
+                }
+                if (task.IsCompletedSuccessfully)
+                {
+                    List<Person> people = task.Result;
+                    foreach (var person in people)
+                    {
+                        PersonListBox.Items.Add(person);
+                    }
+                }
+                FetchWithTaskButton.IsEnabled = true;
+                tokenSource.Dispose();
+            },
+            TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    private async void FetchWithAwaitButton_Click(object sender, RoutedEventArgs e)
+    {
+        ClearListBox();
+        tokenSource = new();
+
+        FetchWithAwaitButton.IsEnabled = false;
+
+        try
+        {
+            List<Person> people = await reader.GetPeopleAsync(tokenSource.Token);
+            foreach (var person in people)
+            {
+                PersonListBox.Items.Add(person);
+            }
+        }
+        catch(OperationCanceledException ex)
+        {
+            MessageBox.Show($"CANCELED\n{ex.GetType()}\n{ex.Message}");
+        }
+        catch(Exception ex)
+        {
+            MessageBox.Show($"ERROR\n{ex.GetType()}\n{ex.Message}");
+        }
+        finally
+        {
+            FetchWithAwaitButton.IsEnabled = true;
+            tokenSource.Dispose();
+        }   
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        tokenSource?.Cancel();
+    }
+
+    private void ClearListBox()
+    {
+        PersonListBox.Items.Clear();
+    }
+}
